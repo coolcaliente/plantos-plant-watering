94d183673aa75231582f00a3d68d208c5a8c9b30
Paul Hodde <paulhodde12@gmail.com>
2018-05-12 11:32:14 -0500

    first draft server

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultlayour: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: ""
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connection: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadID);
+});
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT" + PORT);
+});
