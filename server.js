86c5ccd5cc8bff3313f004fd0ac1df591d222d67
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-05-14 20:51:49 -0500

    added plants and a user

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Root",
+    password: "password",
     database: "hngplants_db"
 });
 
